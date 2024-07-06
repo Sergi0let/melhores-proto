d9ff90f5928379ffb809b803a75f3926ae0a2a38
Sergij Vashkevych <s.vashkevych@gmail.com>
2024-07-06 18:43:36 +0300

    Add header menu

--- js/app.js
+++ js/app.js
@@ -1,5 +1,25 @@
 (() => {
     "use strict";
+    let isMobile = {
+        Android: function() {
+            return navigator.userAgent.match(/Android/i);
+        },
+        BlackBerry: function() {
+            return navigator.userAgent.match(/BlackBerry/i);
+        },
+        iOS: function() {
+            return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+        },
+        Opera: function() {
+            return navigator.userAgent.match(/Opera Mini/i);
+        },
+        Windows: function() {
+            return navigator.userAgent.match(/IEMobile/i);
+        },
+        any: function() {
+            return isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows();
+        }
+    };
     let _slideUp = (target, duration = 500, showmore = 0) => {
         if (!target.classList.contains("_slide")) {
             target.classList.add("_slide");
@@ -220,6 +240,9 @@
             }
         }));
     }
+    function removeClasses(array, className) {
+        for (var i = 0; i < array.length; i++) array[i].classList.remove(className);
+    }
     function uniqArray(array) {
         return array.filter((function(item, index, self) {
             return self.indexOf(item) === index;
@@ -382,6 +405,19 @@
     }
     const da = new DynamicAdapt("max");
     da.init();
+    window.onload = function() {
+        document.addEventListener("click", documentActions);
+        function documentActions(e) {
+            const targetElement = e.target;
+            if (window.innerWidth > 768 && isMobile.any()) {
+                if (targetElement.classList.contains("menu__link")) {
+                    removeClasses(document.querySelectorAll(".menu__item._hover"), "_hover");
+                    targetElement.closest(".menu__item").classList.toggle("_hover");
+                }
+                if (!targetElement.closest(".menu__item") && document.querySelectorAll(".menu__item._hover").length > 0) removeClasses(document.querySelectorAll(".menu__item._hover"), "_hover");
+            }
+        }
+    };
     window["FLS"] = true;
     menuInit();
     spollers();
